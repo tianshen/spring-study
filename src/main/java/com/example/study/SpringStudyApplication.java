4cf14fa7c6eb57a4ee011dd4831f7d9c32028f65
jiwei <eunhyeyoon@126.com>
2024-01-27 15:01:02 +0800

    启动

--- src/main/java/com/example/study/SpringStudyApplication.java
+++ src/main/java/com/example/study/SpringStudyApplication.java
@@ -10,6 +10,7 @@ public class SpringStudyApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringStudyApplication.class, args);
+        System.out.println("-------启动");
     }
 
 }
